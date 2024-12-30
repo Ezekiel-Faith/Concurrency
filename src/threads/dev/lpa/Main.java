2921213fc1152ea28a768995a3e68b78d23795ee
Ezekie-Faith <ezekielfaith2000@gmail.com>
2024-12-30 03:48:30 +0100

    second commit

--- src/threads/dev/lpa/Main.java
+++ src/threads/dev/lpa/Main.java
@@ -2,6 +2,7 @@ package threads.dev.lpa;
 
 public class Main {
 	public static void main(String[] args) {
-
+		var currentThread = Thread.currentThread();
+		System.out.println(currentThread.getClass().getName());
 	}
 }
